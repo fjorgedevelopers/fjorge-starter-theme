f6e190a4353d6f45b16de1eac9f0001a10a9e93c
Laurel Johnson <laurel.johnson@fjorgedigital.com>
2017-02-09 14:22:42 -0600

    restructure sass files and add bourbon and neat sass libraries

--- functions.php
+++ functions.php
@@ -10,7 +10,6 @@ function my_add_theme_scripts() {
 
     // stylesheets for compiling sass on the server at runtime
     wp_enqueue_style( 'style', get_template_directory_uri().'/sass/style.php?p=style.scss' );
-    wp_enqueue_style( 'mobile', get_template_directory_uri().'/sass/style.php?p=mobile.scss' );
 
     // fontawesome
     wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/fonts/font-awesome-4.6.3/css/font-awesome.min.css' );
