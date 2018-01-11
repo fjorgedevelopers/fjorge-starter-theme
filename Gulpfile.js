459fea15bc31e7a4c9842c521ea29bf88df234ef
Laurel Johnson <laurel.johnson@fjorgedigital.com>
2018-01-11 15:31:56 -0600

    add vendor directories for js libraries and css that compile to single minified files, update watch task and add a css-minifier

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,22 +1,43 @@
 // run gulp to compile styles to /css/style.css
 
+/****************************************
+        DEPENDENCIES
+*****************************************/
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var minifycss = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 
+
+/****************************************
+        SOURCE PATHS
+*****************************************/
 // source files
 var input_sass_stylesheet = './src/sass/style.scss';
 var input_sass_components = './src/sass/**/*.scss';
-var input_js = './src/js/**/*.js';
+var input_vendor_css = './src/vendor/css/**/*.css';
 
-// distribution directories
+var input_custom_js =   [
+                            './src/js/main.js',
+                        ];
+var input_vendor_js =  [
+                            './src/vendor/js/**/*.js',
+                        ];                        
+
+
+/****************************************
+        DISTRIBUTION DIRECTORIES
+*****************************************/
 var destination_sass = './dist/css/';
 var destination_js = './dist/js/';
 
-// tasks
+
+/****************************************
+        TASKS
+*****************************************/
 gulp.task('sass', function() {
     return gulp
         .src(input_sass_stylesheet)
@@ -27,44 +48,61 @@ gulp.task('sass', function() {
         })
         .on('error', sass.logError))
         .pipe(autoprefixer())
-        .pipe(gulp.dest(destination_sass));
-});
+        .pipe(gulp.dest(destination_sass))
 
-gulp.task('production', function() {
-    return gulp
-        .src(input_sass_stylesheet)
-        .pipe(sass({ style: 'compressed' }).on('error', sass.logError))
+        // minify for production
+        .pipe(rename('style.min.css'))
+        .pipe(minifycss())
         .pipe(gulp.dest(destination_sass));
 });
 
 gulp.task('scripts', function() {
-return gulp.src(input_js)
-    .pipe(concat('compiled.js'))
+return gulp.src(input_custom_js)
+    .pipe(concat('custom.js'))
+    .pipe(gulp.dest(destination_js))
+
+    // minify for production
+    .pipe(rename('custom.min.js'))
+    .pipe(uglify())
     .pipe(gulp.dest(destination_js));
 });
 
-gulp.task('watch-sass-components', function() {
-    return gulp
-        .watch(input_sass_components, ['sass'])
+gulp.task('vendor-scripts', function() {
+    return gulp.src(input_vendor_js)
+        .pipe(concat('vendor.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(destination_js));
+});
+
+// COMPILE & MINIFY CSS LIBRARIES
+gulp.task('vendor-css', function() {
+    return gulp.src(input_vendor_css)
+        .pipe(concat('vendor.min.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest(destination_sass));
+});
+
+
+/****************************************
+        WATCH TASK
+*****************************************/
+gulp.task('watch', function () {
+    gulp.watch(input_custom_js, ['scripts'])
+        .on('change', function(event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        });
+    gulp.watch(input_vendor_js, ['vendor-scripts'])
+        .on('change', function(event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        });
+    gulp.watch(input_sass_components, ['sass'])
+        .on('change', function(event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        });
+    gulp.watch(input_sass_stylesheet, ['sass'])
         .on('change', function(event) {
             console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
         });
 });
 
-gulp.task('watch-sass-stylesheet', function() {
-    return gulp
-        .watch(input_sass_stylesheet, ['sass'])
-        .on('change', function(event) {
-            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        });
-});
-
-gulp.task('watch-js', function() {
-    return gulp
-        .watch(input_js, ['scripts'])
-        .on('change', function(event) {
-            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        });
-});
-
-gulp.task('default', ['sass', 'scripts', 'watch-sass-components', 'watch-sass-stylesheet', 'watch-js']);
+gulp.task('default', ['sass', 'scripts', 'vendor-scripts', 'vendor-css', 'watch']);
