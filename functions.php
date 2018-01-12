d6d546491a9686a93490b2ee29375d9da1ab9874
Laurel Johnson <laurel.johnson@fjorgedigital.com>
2018-01-12 16:35:41 -0600

    update enqueue function for new filenames and concatenated vendor scripts and styles

--- functions.php
+++ functions.php
@@ -8,8 +8,11 @@
 
 function my_add_theme_scripts() {
 
-    // compiled styles
-    wp_enqueue_style( 'style', get_template_directory_uri().'/dist/css/style.css' );
+    // vendor styles
+    wp_enqueue_style( 'style', get_template_directory_uri().'/dist/css/vendor.min.css' );
+
+    // custom styles
+    wp_enqueue_style( 'style', get_template_directory_uri().'/dist/css/style.min.css' );
 
     // css hotfixes
     wp_enqueue_style( 'hotfix', get_template_directory_uri().'/dist/css/hotfix.css' );
@@ -17,8 +20,12 @@ function my_add_theme_scripts() {
     // fontawesome
     wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/fonts/font-awesome-4.6.3/css/font-awesome.min.css' );
 
-    // scripts
-    wp_register_script( 'compiled-scripts', get_template_directory_uri() . '/dist/js/compiled.js', array('jquery'), '1.0.0', true );
+    // vendor scripts
+    wp_register_script( 'compiled-scripts', get_template_directory_uri() . '/dist/js/vendor.min.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script('compiled-scripts');
+
+    // custom scripts
+    wp_register_script( 'compiled-scripts', get_template_directory_uri() . '/dist/js/custom.min.js', array('jquery'), '1.0.0', true );
     wp_enqueue_script('compiled-scripts');
 
 }
