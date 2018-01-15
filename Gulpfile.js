64cf08b8d0bac70ed28cee385255c42510b8eace
Pat Kavanagh <>
2018-01-15 14:18:16 -0600

    Add `build` gulp task. Update `watch` task to use build instead of individual tasks. Add process comments.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,13 @@
+// Questions? Visit our documentation: http://wjiki.fjorgedigital.com/index.php?title=Gulp
 // run gulp to compile styles to /css/style.css
 
 /****************************************
         DEPENDENCIES
 *****************************************/
+/*
+ * This list of dependency variables comes from the package.json file. Ensure any dependency listed here is also added to package.json.
+ * These variables are declared here at the top and are used throughout the gulpfile to complete certain tasks and add functionality.
+*/
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
@@ -15,22 +20,29 @@ var autoprefixer = require('gulp-autoprefixer');
 /****************************************
         SOURCE PATHS
 *****************************************/
-// source files
+/*
+  You'll also notice there is a number 1 and 2 next to some of the source paths. There are 2 different ways to set up your source files depending on your project's needs:
+  1) The first way is to set your paths with a wildcard: *. This will allow you to add any files to your source directories and gulp will compile all of them.
+  2) The other way is to manually add the file paths you want to be compiled. This will allow you to order certain libraries or custom files for dependency sake. If there is no need to get this granular control, leaving it set to the wildcard makes things rather straightforward during development.
+*/
 var input_sass_stylesheet = './src/sass/style.scss';
 var input_sass_components = './src/sass/**/*.scss';
 var input_vendor_css = './src/vendor/css/**/*.css';
 
 var input_custom_js =   [
-                            './src/js/main.js',
+                            './src/js/*.js', // 1 - used as a catch-all. This will add all .js files located within src/js/ to be compiled.
+                            // './src/js/main.js', // 2 - FOR DEPENDENCIES - if you want to control your enqueue order, manually add each file in the order you'd like
                         ];
 var input_vendor_js =  [
-                            './src/vendor/js/**/*.js',
-                        ];                        
+                            './src/vendor/js/**/*.js', // 1 - used as a catch-all. This will add all .js files located within src/vendor/js/ to be compiled and minfied.
+                            // './src/vendor/js/**/slick.js', // 2 - FOR DEPENDENCIES - if you want to control your enqueue order, manually add each file in the order you'd like
+                        ];
 
 
 /****************************************
         DISTRIBUTION DIRECTORIES
 *****************************************/
+// Location of where compiled production files should be placed
 var destination_sass = './dist/css/';
 var destination_js = './dist/js/';
 
@@ -38,6 +50,8 @@ var destination_js = './dist/js/';
 /****************************************
         TASKS
 *****************************************/
+
+// COMPILE SASS :: UN-MINIFIED & MINIFIED
 gulp.task('sass', function() {
     return gulp
         .src(input_sass_stylesheet)
@@ -56,6 +70,7 @@ gulp.task('sass', function() {
         .pipe(gulp.dest(destination_sass));
 });
 
+// COMPILE CUSTOM JS :: UN-MINIFIED & MINIFIED
 gulp.task('scripts', function() {
 return gulp.src(input_custom_js)
     .pipe(concat('custom.js'))
@@ -67,6 +82,7 @@ return gulp.src(input_custom_js)
     .pipe(gulp.dest(destination_js));
 });
 
+// COMPILE & MINIFY VENDOR JS
 gulp.task('vendor-scripts', function() {
     return gulp.src(input_vendor_js)
         .pipe(concat('vendor.min.js'))
@@ -74,7 +90,7 @@ gulp.task('vendor-scripts', function() {
         .pipe(gulp.dest(destination_js));
 });
 
-// COMPILE & MINIFY CSS LIBRARIES
+// COMPILE & MINIFY VENDOR & MISC CSS
 gulp.task('vendor-css', function() {
     return gulp.src(input_vendor_css)
         .pipe(concat('vendor.min.css'))
@@ -86,6 +102,7 @@ gulp.task('vendor-css', function() {
 /****************************************
         WATCH TASK
 *****************************************/
+// WATCH AND LOG SOURCE FILE CHANGES
 gulp.task('watch', function () {
     gulp.watch(input_custom_js, ['scripts'])
         .on('change', function(event) {
@@ -105,4 +122,8 @@ gulp.task('watch', function () {
         });
 });
 
-gulp.task('default', ['sass', 'scripts', 'vendor-scripts', 'vendor-css', 'watch']);
+// COMPILES SCSS, CSS & JS, does NOT watch for file changes
+gulp.task('build', ['sass', 'scripts', 'vendor-scripts', 'vendor-css']);
+
+// DEFAULT GULP TASK
+gulp.task('default', ['build', 'watch']);
