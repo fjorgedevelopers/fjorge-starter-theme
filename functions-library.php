da31c9ac97a2e25c512aca429cf3104860c7eed5
Brent Reno <brent.reno@fjorgedigital.com>
2019-01-22 08:23:42 -0600

    Update fontawesome to version 5.6.3, and add functions to remove Gutenberg to the functions-library file

--- functions-library.php
+++ functions-library.php
@@ -73,6 +73,23 @@ function get_my_excerpt(){
 }
 
 /*************************************************************/
+/*  Disable Gutenberg Blocks							     */
+/*************************************************************/
+/**
+ *   disable Gutenberg for posts
+ * 
+ * */
+add_filter('use_block_editor_for_post', '__return_false', 10);
+
+/**
+*   disable Gutenberg for post types
+* 
+* */
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
+
+
+
+/*************************************************************/
 /*  something else 											 */
 /*************************************************************/
 
