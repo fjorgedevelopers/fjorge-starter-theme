f2c9234463cec31904cc523491b56ee2a84c755f
Brent Reno <brent.reno@fjorgedigital.com>
2019-03-08 15:21:08 -0600

    rock progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 /** 
      * This is an example of how to comment JS for use with the documentation generation 
+     * @category JQuery
      * @function Doc Ready
      * @param {string} jQueryAlias so $ can be used 
      * @description This document ready syntax supports use of the $ alias in jQuery no conflict mode. See README.md for more info and alternatives.
